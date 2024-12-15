516dfd541b39d02f24bf6ebd1ab4cefadc443067
guzal <jamolovagguzal@gmail.com>
2024-12-15 12:51:08 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from './../assets/react.svg'
+
+// rafce
+const Header = () => {
+  return (
+    <header className="header">
+        <img src={logo} alt="" />
+    </header>
+  )
+}
+
+export default Header
